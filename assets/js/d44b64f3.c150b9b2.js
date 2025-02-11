851462cc60621400aed03e6de8c6a30392ee3023
Guilherme Franco Oliveira <50753090+Guilhermefoliveira@users.noreply.github.com>
2025-02-11 11:16:52 -0300

    Deploy website - based on 0f5e4bfa9aa08ef227cfb72434437a8a04af0803

--- assets/js/d44b64f3.c150b9b2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7398],{7643:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/scc4/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
